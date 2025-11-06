c9c43bcd25a4cf449aca72a543662af9962f4db4
Airike Jaska <95303654+airikej@users.noreply.github.com>
2025-11-06 09:11:39 +0200

    chore: fix storybook logo url #000

--- .storybook/tehik-theme.js
+++ .storybook/tehik-theme.js
@@ -1,20 +1,20 @@
-import { create } from '@storybook/theming';
+import { create } from "@storybook/theming";
 
 export default create({
-  base: 'light',
-  brandTitle: 'Tehik Angular components',
-  brandUrl: 'https://tehik-ee.github.io/tedi-design-system/',
-  brandImage: 'tehik_logo.png',
-  brandTarget: '_self',
+  base: "light",
+  brandTitle: "Tehik Angular components",
+  brandUrl: "https://storybook.tedi.ee/angular",
+  brandImage: "tehik_logo.png",
+  brandTarget: "_self",
 
-  appBg: '#e7f0f6',
-  colorPrimary: '#ffe6cc',
-  colorSecondary: '#005aa3',
+  appBg: "#e7f0f6",
+  colorPrimary: "#ffe6cc",
+  colorSecondary: "#005aa3",
 
-  textColor: '#151926',
-  currentColor: '#151926',
-  textInverseColor: '#151926',
+  textColor: "#151926",
+  currentColor: "#151926",
+  textInverseColor: "#151926",
 
-  barTextColor: '#151926',
-  textMutedColor: '#151926',
+  barTextColor: "#151926",
+  textMutedColor: "#151926",
 });
