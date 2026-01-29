53558446861cf960f94918ac4cec5113019b2982
pesarv <54806197+pesarv@users.noreply.github.com>
2026-01-29 15:24:38 +0200

    fix(vertical-stepper): add missing export #254 (#309)
    * feat(vertical-stepper): add community vertical stepper component #254
    
    * feat(vertical-stepper): emit item select event on router link active change, minor style fixes #254
    
    * feat(vertical-stepper): add vertical stepper a11y attributes #254
    
    * feat(vertical-stepper): add vertical stepper stories #254
    
    * feat(vertical-stepper): update aria attributes, remove unused css variables #254
    
    * feat(vertical-stepper): add list roles for item count, remove redundant css selector #254
    
    * feat(vertical-stepper): add missing export #254
    
    ---------
    
    Co-authored-by: pearu.sarv <pearu.sarv@twn.ee>

--- community/components/navigation/index.ts
+++ community/components/navigation/index.ts
@@ -2,3 +2,4 @@ export * from "./breadcrumbs/breadcrumbs.component";
 export * from "./pagination/pagination.component";
 export * from "./table-of-contents";
 export * from "./tabs";
+export * from "./vertical-stepper";
