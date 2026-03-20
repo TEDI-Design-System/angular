cf3abda1f3a3a1d71044e91c86bd88435e97ae8e
Märt Sessman <mart.sessman@bitweb.ee>
2026-03-20 10:06:11 +0200

    feat(docs): added css-utilities documentation #368 (#369)
    Co-authored-by: m2rt <mart@bitweb.ee>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
     "../src/docs/get-started.mdx",
     "../src/docs/changelog.mdx",
     "../src/docs/badges.mdx",
+    "../src/docs/css-utilities.mdx",
     "../src/docs/colors/colors.mdx",
     "../src/docs/**/*.stories.@(js|jsx|mjs|ts|tsx)",
     "../tedi/**/*.stories.@(js|jsx|mjs|ts|tsx)",
