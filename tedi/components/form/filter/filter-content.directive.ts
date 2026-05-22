e06a64c2e99dd03122901060e678776c2dcd1a30
m2rt <mart.sessman@bitweb.ee>
2026-05-22 09:29:49 +0300

    chore: removed duplicate filter package as a merge leftover #374

--- tedi/components/form/filter/filter-content.directive.ts
+++
@@ -1,7 +0,0 @@
-import { Directive } from "@angular/core";
-
-@Directive({
-  selector: "[tediFilterContent]",
-  standalone: true,
-})
-export class FilterContentDirective {}
--- tedi/components/form/filter/index.ts
+++
@@ -1,9 +0,0 @@
-export {
-  FilterComponent,
-  FilterVariant,
-  FilterSize,
-  FilterOption,
-} from "./filter.component";
-export { FilterContentDirective } from "./filter-content.directive";
-export { FilterPrependDirective } from "./filter-prepend.directive";
-export { FilterGroupComponent } from "./filter-group.component";
