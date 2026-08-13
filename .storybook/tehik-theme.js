36da96a605cd072e6aaf8a628d073bb52f70bd48
Rando Leppik <rando.leppik@bitweb.ee>
2026-08-13 10:07:31 +0300

    chore: replace brand logo #87

--- .storybook/tehik-theme.js
+++ .storybook/tehik-theme.js
@@ -4,7 +4,7 @@ export default create({
   base: "light",
   brandTitle: "Tehik Angular components",
   brandUrl: "https://storybook.tedi.ee/angular",
-  brandImage: "tehik_logo.png",
+  brandImage: "TEDI-logo-sinine.svg",
   brandTarget: "_self",
 
   appBg: "#e7f0f6",
