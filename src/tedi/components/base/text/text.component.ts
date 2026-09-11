cfcc44217ebac0721ec4640773844fecd13f580a
Rando Leppik <rando.leppik@bitweb.ee>
2026-09-11 11:25:40 +0300

    refactor(text): extract heading level types from text modifiers #1

--- src/tedi/components/base/text/text.component.ts
+++ src/tedi/components/base/text/text.component.ts
@@ -5,13 +5,12 @@ import {
   input,
 } from "@angular/core";
 
+export type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6;
+
+export type HeadingModifiers = `h${HeadingLevel}`;
+
 export type TextModifiers =
-  | "h1"
-  | "h2"
-  | "h3"
-  | "h4"
-  | "h5"
-  | "h6"
+  | HeadingModifiers
   | "normal"
   | "small"
   | "extra-small"
